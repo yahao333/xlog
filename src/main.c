8d76e1b6ad274a413411a6f28450b06c0f66f3a9
herry <fexeak@163.com>
2020-11-09 16:27:12 +0800

    add syslog

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 int main(){
     //int priority = atoi( NULL == getenv( "XLOGLEVEL" ) ? "4" : getenv( "XLOGLEVEL" ) );
     //xlevel_set(priority);
+    xlevel_set(7);
     LOGV("log v");
     LOGD("log d [%d]", 13);
     LOGI("log i");
--- src/xlog.c
+++ src/xlog.c
@@ -10,12 +10,17 @@
 
 static char logMsg[1024];
 static xlog_level_t xlog_default_level_ = XLOG_VERBOSE;
+static xlog_mode_t xlog_default_mode_ = OUTPUT_TO_TTY;
 
 void xlog_writex(xlog_level_t level,const char* format, va_list args)
 {
     if(level <= xlog_default_level_){
         vsprintf(logMsg, format,  args);
-        printf("%s", logMsg);
+        if(OUTPUT_TO_TTY == xlog_default_mode_){
+            printf("%s", logMsg);
+        }else if(OUTPUT_TO_SYSLOG == xlog_default_mode_){
+            syslog(level, "%s", logMsg);
+        }
     }
 }
 
@@ -23,11 +28,7 @@ void xlog_write(xlog_level_t level, const char* format, ...)
 {
     va_list va;
     va_start(va, format);
-    //xlog_writex(level, format, va);
-    if(level <= xlog_default_level_){
-        vsprintf(logMsg, format,  va);
-        printf("%s", logMsg);
-    }
+    xlog_writex(level, format, va);
     va_end(va);
 }
 
